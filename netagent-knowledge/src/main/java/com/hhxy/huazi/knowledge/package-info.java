3535c0c2fd8efd7a949e30dc38e49ff4f87b1605
huazi.wzh <15512975218@163.com>
2026-09-25 11:55:22 +0800

    初始化项目结构，确认要做那几个模块

---
+++ netagent-knowledge/src/main/java/com/hhxy/huazi/knowledge/package-info.java
@@ -0,0 +1,1 @@
+package com.hhxy.huazi.knowledge;
---
+++ netagent-starter/src/main/java/com/hhxy/huazi/NetAgentApplication.java
@@ -0,0 +1,12 @@
+package com.hhxy.huazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetAgentApplication.class, args);
+    }
+}
